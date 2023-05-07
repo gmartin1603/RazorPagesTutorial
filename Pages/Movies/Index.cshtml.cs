01aaf603dc30e7230c4957d872dfe1e883e7d48a
George <duramaxer228@gmail.com>
2023-05-07 11:25:43 -0500

    Add project files.

---
+++ Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesTutorial.Data;
+using RazorPagesTutorial.Models;
+
+namespace RazorPagesTutorial.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPagesTutorial.Data.RazorPagesTutorialContext _context;
+
+        public IndexModel(RazorPagesTutorial.Data.RazorPagesTutorialContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Movie != null)
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
+        }
+    }
+}
